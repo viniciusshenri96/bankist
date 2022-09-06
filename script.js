394e26b746f3814a373164072aab96958410c40b
Vinicius <vinicius_henrique96@hotmail.com>
2022-09-06 12:13:52 -0300

    Implementando a funcionalidade Request loan

--- script.js
+++ script.js
@@ -150,6 +150,22 @@ btnTransfer.addEventListener('click', function(e){
 
 });
 
+btnLoan.addEventListener('click', function(e){
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if(amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+    inputLoanAmount.value = '';
+  };
+
+})
+
 btnClose.addEventListener('click', function(e){
   e.preventDefault();
 
@@ -166,3 +182,4 @@ btnClose.addEventListener('click', function(e){
   // Clear input fields
   inputCloseUsername.value = inputClosePin.value = '';
 });
+
